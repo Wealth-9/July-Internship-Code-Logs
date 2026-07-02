96a33f7b880a55d7c1c0ba2c3a70a299d2164521
vickycodes <tabithaede838@gmail.com>
2026-07-02 10:12:17 +0100

    I am learning how to use NetworkX Library for Graph,An undirected graphs with nodes

---
+++ IntrotoNetworkX.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+G=nx.Graph()
+G.add_node("A")
+G.add_node("B")
+G.add_node("C")
+G.add_node("D")
+G.add_node("E")
+
+G.add_edge("A","C")
+G.add_edge("B","C")
+G.add_edge("B","D")
+G.add_edge("C","D")
+G.add_edge("D","E")
+
+
+nx.draw(G,with_labels=True, node_color="red", node_size=3000, font_color="white",font_family="Times New Roman",font_weight="bold", width=5 )
+plt.margins(0.2)
+plt.show()
