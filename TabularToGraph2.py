e4da5a01b81d73753d2399018d04076c1bce9430
vickycodes <tabithaede838@gmail.com>
2026-07-03 13:28:19 +0100

    Turning to a graph and inspecting to see it work

--- TabularToGraph2.py
+++ TabularToGraph2.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import numpy as np
+import networkx as nx
 
+
+
+#creating my own random dataset using pandas
 np.random.seed(42) #for same output 
 
 accounts = ["A","B","C","D","E"]
@@ -14,3 +18,14 @@ df = pd.DataFrame({
 })
 
 print(df)
+# Converting Tabular data to a graph.
+G = nx.from_pandas_edgelist( #reads dataframe row by row and builds a graph
+  df,
+  source="sender", #the starting point/nodes
+  target="receiver", #ending column/arrow head
+  edge_attr="amount", # add attribute amountto edge data
+  create_using=nx.DiGraph()  #a directed graph as money flows from sender to reciever if not NetworkX defaults to an undirected graph
+ )
+
+print("Nodes:",G.nodes())
+print("Edges:", G.edges(data=True))
